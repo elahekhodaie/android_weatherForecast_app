9d9c8f20897931e3d87f584ecd04bad40eef34c2
sinatav <alirezatavakkoli@ymail.com>
2020-04-04 15:00:13 +0430

    something fixed

---
+++ app/src/main/java/com/example/mobile_weatherforecast_app/MainActivity.java
@@ -0,0 +1,210 @@
+package com.example.mobile_weatherforecast_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+// the mapBox geo coding api will be used in this project which the forwarding geo coding will takes
+// a String which here is a city name and turns it into coordinates
+
+
+//----------------------------------------------------------------------------------------------------
+// keys :
+// geo coding map box secret key is : pk.eyJ1IjoiZWxhaGVraG9kYWllIiwiYSI6ImNrOGJ0YzFsMzBmdHAza3BpNHR1dmdzZnoifQ.Hq12JjRjpv90sGEjOMyy0g
+// dark sky api secret key is :3bed00074e5a1aef2e2cbd65e6dafb77
+
+//----------------------------------------------------------------------------------------------------
+public class MainActivity extends AppCompatActivity {
+
+
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        final EditText simpleEditText = (EditText) findViewById(R.id.cityName);//get the id for edit text
+//        Button showCoordButton = (Button) findViewById(R.id.angry_btn);//get the id for button
+//
+//        showCoordButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//               // new GetCoordinates().execute(simpleEditText.getText().toString());
+//            }
+//        });
+//
+//
+//    }
+// this function is to get the text field info and start fetching info
+
+
+String cityName ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+
+        final EditText enterCityNameField = (EditText) findViewById(R.id.cityName);
+        Button showWeatherButton = (Button) findViewById(R.id.angry_btn) ;
+
+
+        String CITY = enterCityNameField.getText().toString();
+        String API = "8118ed6ee68db2debfaaa5a44c832918";
+
+        setName(cityName, CITY);
+
+
+    }
+
+    public void sendCityName (View view){
+
+        final EditText enterCityNameField = (EditText) findViewById(R.id.cityName);
+        String cityName = enterCityNameField.getText().toString();
+
+        Intent getWeather = new Intent(this, WeatherActivity.class);
+        final  int resuly = 1;
+        getWeather.putExtra("CITY",cityName);
+        startActivity(getWeather);
+
+
+
+//-------------------------------------------
+
+    }
+
+    public void setName(String cityName, String textEdit){
+        cityName = textEdit;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------
+
+    // this part is for the fetching coordinates from forward geoCoding map box Api
+
+
+//public void getWeather(){
+//    final EditText editText = (EditText) findViewById(R.id.cityName);
+//    editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//        @Override
+//        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+//            boolean handled = false;
+//
+//            if(i == EditorInfo.IME_ACTION_SEND){
+//                //TODO  fetch city location from map box api and pass it to dark sky API
+//                handled = true;
+//                getInfoTextField(editText);
+//            }
+//
+//            return handled;
+//        }
+//    });
+//
+//}
+//   public void getInfoTextField(EditText editText){
+//        String cityName = editText.getText().toString();
+//        //TODO  use the cityName and map box api to find location
+//
+//
+//   }
+//
+//   private class  GetCoordinates extends AsyncTask<String, Void, String >{
+//
+//
+//        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+//
+//        @Override
+//        protected void onPreExecute(){
+//            super.onPreExecute();
+//            progressDialog.setMessage("Please wait ....");
+//            progressDialog.setCanceledOnTouchOutside(false);
+//            progressDialog.show();
+//        }
+//       @Override
+//       protected String doInBackground(String...  strings) {
+//            String response;
+//            try{
+//                String address = strings[0];
+//                HttpDataHandler http = new HttpDataHandler();
+//                String url = String.format("https://api.mapbox.com/geocoding/v5/mapbox.places/%s.json?access_token=%s", address);
+//                response = http.getHttpData(url);
+//                return response;
+//
+//            }
+//            catch (Exception e){
+//                e.printStackTrace();
+//            }
+//           return null;
+//       }
+//
+//
+//
+//       @Override
+//       protected void onPostExecute(String s){
+//            try {
+//                JSONObject jsonObject = new JSONObject(s);
+//                String lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry").
+//                        getJSONObject("location").get("lat").toString();
+//
+//                String lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry").
+//                        getJSONObject("location").get("lng").toString();
+//                textCoordx = lat;
+//                textCoordy = lng;
+//                if(progressDialog.isShowing())
+//                    progressDialog.dismiss();
+//
+//                System.out.println("the city coordination is :" + lat + "-" + lng);
+//
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//       }
+//
+//   }
+
+
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/mobile_weatherforecast_app/WeatherActivity.java
@@ -0,0 +1,171 @@
+package com.example.mobile_weatherforecast_app;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+//better naming scheme
+public class WeatherActivity extends Activity {
+    String CITY;
+
+
+    TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
+            sunsetTxt, windTxt, pressureTxt, humidityTxt;
+
+
+
+//
+//    public void setAPI(String API) {
+//        this.API = API;
+//    }
+//
+//    public void setCITY(String CITY) {
+//        this.CITY = CITY;
+   // }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        Intent intent = getIntent();
+         CITY = intent.getStringExtra("CITY");
+
+        addressTxt = findViewById(R.id.address);
+        updated_atTxt = findViewById(R.id.updated_at);
+        statusTxt = findViewById(R.id.status);
+        tempTxt = findViewById(R.id.temp);
+        temp_minTxt = findViewById(R.id.temp_min);
+        temp_maxTxt = findViewById(R.id.temp_max);
+        sunriseTxt = findViewById(R.id.sunrise);
+        sunsetTxt = findViewById(R.id.sunset);
+        windTxt = findViewById(R.id.wind);
+        pressureTxt = findViewById(R.id.pressure);
+        humidityTxt = findViewById(R.id.humidity);
+
+        weatherTask wt = new weatherTask();
+        wt.execute();
+
+
+
+    }
+
+
+
+
+    @SuppressLint("StaticFieldLeak")
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById(R.id.loader).setVisibility(View.VISIBLE);
+            findViewById(R.id.mainContainer).setVisibility(View.GONE);
+            findViewById(R.id.errorText).setVisibility(View.GONE);
+        }
+
+        protected String doInBackground(String... args) {
+           // String CITY = "tehran";
+            String API = "8118ed6ee68db2debfaaa5a44c832918";
+
+            String response;
+            HttpDataHandler http = new HttpDataHandler();
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API;
+            response = http.getHttpData(url);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                String pressure = main.getString("pressure");
+                String humidity = main.getString("humidity");
+
+                long sunrise = sys.getLong("sunrise");
+                long sunset = sys.getLong("sunset");
+                String windSpeed = wind.getString("speed");
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+                addressTxt.setText(address);
+                updated_atTxt.setText(updatedAtText);
+                statusTxt.setText(weatherDescription.toUpperCase());
+                tempTxt.setText(temp);
+                temp_minTxt.setText(tempMin);
+                temp_maxTxt.setText(tempMax);
+                sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+                sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+                windTxt.setText(windSpeed);
+                pressureTxt.setText(pressure);
+                humidityTxt.setText(humidity);
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+
+
+            } catch (JSONException e) {
+                findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+            }
+
+        }
+    }
+
+
+}
